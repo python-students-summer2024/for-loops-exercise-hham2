3f2d46e29c9cc057aceac799e2eb896e26531bba
Henrik Hamilton <henrikham14@gmail.com>
2024-06-13 10:34:22 -0400

    in create_turtle() in main(), range should be (-100, 0, 25) to create four squares. Was originally (-200, 0, 25).

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ else:
 from loopy_turtles import create_turtle, draw_square, draw_star
 
 t = create_turtle("red", "yellow")  # create a turtle object
-for x in range(-200, 0, 25):  # loop four times
+for x in range(-100, 0, 25):  # loop four times
     draw_square(t, x, x, 100, "left", "#F5DEB3")  # draw a square
 
 draw_star(t, 200, 200, 100, 120, "right", "red")  # draw a five-pointed star
